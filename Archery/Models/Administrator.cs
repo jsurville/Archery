d3b3487fb0e31d2b2570f85a195c945b7c691e20
jsurville <julien.surville@gmail.com>
2018-09-26 09:56:07 +0200

    Création des Classes Models métier

---
+++ Archery/Models/Administrator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Archery.Models
+{
+    public sealed class Administrator:User // "sealed" dont on ne peut hériter
+    {
+
+    }
+}
