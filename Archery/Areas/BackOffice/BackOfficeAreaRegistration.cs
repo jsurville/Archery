20ceb4b7922d6b4550692a9bbdc8c09770602e5c
jsurville <julien.surville@gmail.com>
2018-10-11 18:54:01 +0200

    Recherche suite

--- Archery/Areas/BackOffice/BackOfficeAreaRegistration.cs
+++ Archery/Areas/BackOffice/BackOfficeAreaRegistration.cs
@@ -17,7 +17,7 @@ namespace Archery.Areas.BackOffice
             context.MapRoute(
                 "BackOffice_default",
                 "BackOffice/{controller}/{action}/{id}",
-                new {  controller = "Dashboard", action = "Index", id = UrlParameter.Optional }
+                new {controller="Dashboard", action = "Index", id = UrlParameter.Optional }
             );
             
         }
